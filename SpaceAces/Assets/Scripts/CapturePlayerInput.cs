91d06f9ed5d718f2e3070b05d159ed9224bcd269
sounderdisc <mattbhubbs@knights.ucf.edu>
2022-02-22 22:58:03 -0500

    generic controller and joystick input works

--- SpaceAces/Assets/Scripts/CapturePlayerInput.cs
+++ SpaceAces/Assets/Scripts/CapturePlayerInput.cs
@@ -1,48 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CapturePlayerInput : MonoBehaviour
 {
-    public FlightControlV2 flightController;
+    public FlightControl flightController;
+    public PlayerControls controls;
 
-    // These are all instance variables so we can set them in editor. Pls dont delete even though they arent used anywhere
+    // These are all instance variables so we can set them in editor. Passed to FlightControl
     public float baseThrust;
     public float baseRotation;
     public float rollMod, pitchMod, yawMod, surgeMod, swayMod, heaveMod;
+    private float surgeInput, swayInput, heaveInput;
+    private float rollInput, pitchInput, yawInput;
+    private bool toggleFA;
     
     // Start is called before the first frame update
     void Start()
     {
         // PSA using a normal constructor for a monobehavior is a no-no. use add component instead
-        flightController = gameObject.AddComponent<FlightControlV2>() as FlightControlV2; // CORRECT
+        flightController = gameObject.AddComponent<FlightControl>() as FlightControl;
         flightController.FakeConstructor(rollMod, pitchMod, yawMod, surgeMod, swayMod, heaveMod, baseThrust, baseRotation);
     }
 
+    void Awake()
+    {
+        controls = new PlayerControls();
+
+        // toggle flight assist
+        controls.ShipMovement.ToggleFA.performed += ctx => toggleFA = !toggleFA;
+
+        // TRANSLATIONAL AXES
+        controls.ShipMovement.SurgeAxis.performed += ctx => surgeInput = ctx.ReadValue<float>();
+        controls.ShipMovement.SwayAxis.performed += ctx => swayInput = ctx.ReadValue<float>();
+        controls.ShipMovement.HeaveAxis.performed += ctx => heaveInput = ctx.ReadValue<float>();
+
+        controls.ShipMovement.SurgeAxis.canceled += ctx => surgeInput = 0f;
+        controls.ShipMovement.SwayAxis.canceled += ctx => swayInput = 0f;
+        controls.ShipMovement.HeaveAxis.canceled += ctx => heaveInput = 0f;
+
+        // ROTATIONAL AXES
+        controls.ShipMovement.RollAxis.performed += ctx => rollInput = ctx.ReadValue<float>();
+        controls.ShipMovement.PitchAxis.performed += ctx => pitchInput = ctx.ReadValue<float>();
+        controls.ShipMovement.YawAxis.performed += ctx => yawInput = ctx.ReadValue<float>();
+
+        controls.ShipMovement.RollAxis.canceled += ctx => rollInput = 0f;
+        controls.ShipMovement.PitchAxis.canceled += ctx => pitchInput = 0f;
+        controls.ShipMovement.YawAxis.canceled += ctx => yawInput = 0f;
+    }
+
+    void OnEnable()
+    {
+        controls.ShipMovement.Enable();
+        controls.Weapons.Enable();
+    }
+
+    void OnDisable()
+    {
+        controls.ShipMovement.Disable();
+        controls.Weapons.Disable();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        // TRANSLATIONAL AXES
-        float surgeInput = Input.GetAxis("SurgeAxis");
-        float swayInput = Input.GetAxis("SwayAxis");
-        float heaveInput = Input.GetAxis("HeaveAxis");
-
-        // ROTATIONAL AXES
-        float rollInput = Input.GetAxis("RollAxis");
-        float pitchInput = Input.GetAxis("PitchAxis");
-        float yawInput = Input.GetAxis("YawAxis");
-
-        // flight stabilization assist toggle
-        bool toggleFA = false;
-        // if (Input.GetKeyDown("left shift"))
-        // {
-        //     toggleFA = true;
-        // }
-        if (Input.GetAxis("toggleFA") > 0)
-        {
-            toggleFA = true;
-        }
-
         flightController.ReceiveInput(toggleFA, surgeInput, swayInput, heaveInput, rollInput, pitchInput, yawInput);
     }
 }
--- SpaceAces/Assets/Scripts/FlightControl.cs
+++ SpaceAces/Assets/Scripts/FlightControl.cs
@@ -4,104 +4,79 @@ using UnityEngine;
 
 public class FlightControl : MonoBehaviour
 {
-    // instance variables can be changed in inspector
-    Rigidbody m_Rigidbody;
-    public float m_Thrust; // recomended value in inspector: 20
-    public float m_Torque; // recomended value in inspector: 5
-    
-    // Start is called before the first frame update
+    public float baseThrust;
+    public float baseRotation;
+    public float rollMod, pitchMod, yawMod, surgeMod, swayMod, heaveMod;
+    private float surgeInput, swayInput, heaveInput;
+    private float rollInput, pitchInput, yawInput;
+    public Rigidbody rb;
+    public bool isActiveFA; // like auto break in nav meshes mentioned in class 
+
     void Start()
     {
-        //Fetch the Rigidbody from the GameObject with this script attached
-        m_Rigidbody = GetComponent<Rigidbody>(); // https://docs.unity3d.com/ScriptReference/Rigidbody.AddForce.html
+        // Fetch the Rigidbody from the GameObject with this script attached
+        rb = GetComponent<Rigidbody>(); // https://docs.unity3d.com/ScriptReference/Rigidbody.AddForce.html
     }
 
-    // Update is called once per frame
-    void Update()
+    // constructor
+    public void FakeConstructor(float roll, float pitch, float yaw, float surge, float sway, float heave, float thrust, float rotation)
     {
-        // empty, for now
+        rollMod = roll;
+        pitchMod = pitch;
+        yawMod = yaw;
+
+        surgeMod = surge;
+        swayMod = sway;
+        heaveMod = heave;
+
+        baseThrust = thrust;
+        baseRotation = rotation;
     }
+
+    //here's where you're gonna update thrust with the stick's position, or the AI pilot's commands
+    public void ReceiveInput(bool toggleFA, float surge, float sway, float heave, float roll, float pitch, float yaw)
+    {
+    	// I know leaving commented code in isnt good practice, but this is nice for debugging, so I'll leave it
+        // print("in ReceiveInput(surge " + surge + ", sway " + sway + ", heave " + heave + ", roll " + roll + ", pitch " + pitch + ", yaw " + yaw + ")");
+        // flight assist toggle
+        if (toggleFA != isActiveFA)
+        {
+            if (!isActiveFA)
+            {
+                isActiveFA = true;
+                rb.drag = baseThrust / 5;
+                rb.angularDrag = baseRotation * 1.5f;
+            }
+            else
+            {
+                isActiveFA = false;
+                rb.drag = 0;
+                rb.angularDrag = 0;
+            }
+        }
+
+        // TRANSLATIONAL AXES
+        surgeInput = surge;
+        swayInput = sway;
+        heaveInput = heave;
+
+        // ROTATIONAL AXES
+        rollInput = roll;
+        pitchInput = pitch;
+        yawInput = yaw;
+    }
+
     void FixedUpdate()
     {
-        //------ translation axes ----------
-        // surge, forwards and back
-        if (Input.GetKey(KeyCode.W))
-        {
-            print("W key was pressed");
-            m_Rigidbody.AddForce(transform.forward * m_Thrust);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            print("S key was pressed");
-            m_Rigidbody.AddForce((-1*transform.forward) * m_Thrust);
-        }
+        // TRANSLATIONAL AXES
+        print("in FixedUpdate " + (surgeMod * surgeInput * baseThrust));
+        rb.AddRelativeForce(Vector3.forward * surgeMod * surgeInput * baseThrust);
+        rb.AddRelativeForce(Vector3.right * swayMod * swayInput * baseThrust);
+        rb.AddRelativeForce(Vector3.up * heaveMod * heaveInput * baseThrust);
         
-        // sway, left and right
-        if (Input.GetKey(KeyCode.A))
-        {
-            print("A key was pressed");
-            m_Rigidbody.AddForce((-1*transform.right) * m_Thrust);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            print("D key was pressed");
-            m_Rigidbody.AddForce(transform.right * m_Thrust);
-        }
-
-        // Heave, up and down
-        if (Input.GetKey(KeyCode.Q))
-        {
-            print("Q key was pressed");
-            m_Rigidbody.AddForce(transform.up * m_Thrust);
-        }
-
-        if (Input.GetKey(KeyCode.E))
-        {
-            print("E key was pressed");
-            m_Rigidbody.AddForce((-1*transform.up) * m_Thrust);
-        }
-
-        //------ rotational axes ----------
-        
-        // roll, side to side
-        if (Input.GetKey(KeyCode.J))
-        {
-            print("J key was pressed");
-            m_Rigidbody.AddTorque(transform.forward * m_Torque);
-        }
-
-        if (Input.GetKey(KeyCode.L))
-        {
-            print("L key was pressed");
-            m_Rigidbody.AddTorque((-1*transform.forward) * m_Torque);
-        }
-
-        // pitch, up and down
-        if (Input.GetKey(KeyCode.I))
-        {
-            print("I key was pressed");
-            m_Rigidbody.AddTorque(transform.right * m_Torque);
-        }
-
-        if (Input.GetKey(KeyCode.K))
-        {
-            print("K key was pressed");
-            m_Rigidbody.AddTorque((-1*transform.right) * m_Torque);
-        }
-
-        // yaw, left and right
-        if (Input.GetKey(KeyCode.U))
-        {
-            print("U key was pressed");
-            m_Rigidbody.AddTorque((-1*transform.up) * m_Torque);
-        }
-
-        if (Input.GetKey(KeyCode.O))
-        {
-            print("O key was pressed");
-            m_Rigidbody.AddTorque(transform.up * m_Torque);
-        }
+        //ROTATIONAL AXES
+        rb.AddRelativeTorque(Vector3.back * rollMod * rollInput * baseRotation);
+        rb.AddRelativeTorque(Vector3.left * pitchMod * pitchInput * baseRotation);
+        rb.AddRelativeTorque(Vector3.up * yawMod * yawInput * baseRotation);
     }
 }
