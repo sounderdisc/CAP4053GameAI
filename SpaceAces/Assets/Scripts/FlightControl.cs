c9c24899f490efa6083f1e8260698a9582551390
sounderdisc <mattbhubbs@knights.ucf.edu>
2022-02-26 19:15:07 -0500

    initial refactor of flight assist. 90 degree bug known

--- SpaceAces/Assets/Scripts/FlightControl.cs
+++ SpaceAces/Assets/Scripts/FlightControl.cs
@@ -1,15 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System; // Math is in here. I use Math.abs()
 using UnityEngine;
 
 public class FlightControl : MonoBehaviour
 {
-    [SerializeField] float baseThrust;
-    [SerializeField] float baseRotation;
-    [SerializeField] float rollMod, pitchMod, yawMod, surgeMod, swayMod, heaveMod;
+    private float floatZeroTolerance = 0.000001f;
+    private float baseThrust;
+    private float baseRotation;
+    private float rollMod, pitchMod, yawMod, surgeMod, swayMod, heaveMod;
     private float surgeInput, swayInput, heaveInput;
     private float rollInput, pitchInput, yawInput;
-    [SerializeField] Rigidbody rb;
+    private Rigidbody rb;
     private bool isActiveFA; // like auto break in nav meshes mentioned in class 
 
     void Start()
@@ -36,22 +38,20 @@ public class FlightControl : MonoBehaviour
     //here's where you're gonna update thrust with the stick's position, or the AI pilot's commands
     public void ReceiveInput(bool toggleFA, float surge, float sway, float heave, float roll, float pitch, float yaw)
     {
-    	// I know leaving commented code in isnt good practice, but this is nice for debugging, so I'll leave it
-        // print("in ReceiveInput(surge " + surge + ", sway " + sway + ", heave " + heave + ", roll " + roll + ", pitch " + pitch + ", yaw " + yaw + ")");
         // flight assist toggle
         if (toggleFA != isActiveFA)
         {
             if (!isActiveFA)
             {
                 isActiveFA = true;
-                rb.drag = baseThrust / 5;
-                rb.angularDrag = baseRotation * 1.5f;
+                // rb.drag = baseThrust / 5;
+                // rb.angularDrag = baseRotation * 1.5f;
             }
             else
             {
                 isActiveFA = false;
-                rb.drag = 0;
-                rb.angularDrag = 0;
+                // rb.drag = 0;
+                // rb.angularDrag = 0;
             }
         }
 
@@ -69,14 +69,39 @@ public class FlightControl : MonoBehaviour
     void FixedUpdate()
     {
         // TRANSLATIONAL AXES
-        print("in FixedUpdate " + (surgeMod * surgeInput * baseThrust));
         rb.AddRelativeForce(Vector3.forward * surgeMod * surgeInput * baseThrust);
         rb.AddRelativeForce(Vector3.right * swayMod * swayInput * baseThrust);
         rb.AddRelativeForce(Vector3.up * heaveMod * heaveInput * baseThrust);
         
-        //ROTATIONAL AXES
+        // ROTATIONAL AXES
         rb.AddRelativeTorque(Vector3.back * rollMod * rollInput * baseRotation);
         rb.AddRelativeTorque(Vector3.left * pitchMod * pitchInput * baseRotation);
         rb.AddRelativeTorque(Vector3.up * yawMod * yawInput * baseRotation);
+
+        // FLIGHT ASSIST
+        if (isActiveFA)
+        {
+            Vector3 currentAngularVelocity = rb.angularVelocity; 
+            if ((Math.Abs(rollInput) < floatZeroTolerance) && (Math.Abs(currentAngularVelocity.z) > floatZeroTolerance))
+            {
+                float rollSign = currentAngularVelocity.z / Math.Abs(currentAngularVelocity.z);
+                rb.AddRelativeTorque(Vector3.back * rollMod * baseRotation * rollSign);
+            }
+            if ((Math.Abs(pitchInput) < floatZeroTolerance) && (Math.Abs(currentAngularVelocity.x) > floatZeroTolerance))
+            {
+                float pitchSign = currentAngularVelocity.x / Math.Abs(currentAngularVelocity.x);
+                rb.AddRelativeTorque(Vector3.left * pitchMod * baseRotation * pitchSign);
+            }
+            if ((Math.Abs(yawInput) < floatZeroTolerance) && (Math.Abs(currentAngularVelocity.y) > floatZeroTolerance))
+            {
+                float yawSign = -1 * currentAngularVelocity.y / Math.Abs(currentAngularVelocity.y);
+                rb.AddRelativeTorque(Vector3.up * yawMod * baseRotation * yawSign);
+            }
+            print(currentAngularVelocity);
+        }
+        else
+        {
+            print("FA OFF" + rb.angularVelocity);
+        }
     }
 }
