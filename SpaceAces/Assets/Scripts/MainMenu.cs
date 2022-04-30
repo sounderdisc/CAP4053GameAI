7e4582c73754a1d38c0da4a82ed795f1b5174728
root <root@Austin-PC.localdomain>
2022-04-30 15:16:31 -0400

    3 levels, skybox/lighting, music

--- SpaceAces/Assets/Scripts/MainMenu.cs
+++ SpaceAces/Assets/Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("MothershipScene");
+        SceneManager.LoadScene("Level1");
     }
 
     public void QuitGame()
